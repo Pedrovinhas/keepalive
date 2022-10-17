e40dacf842d7c44a68a16070da24940143bc5629
pedrovinhas <pstvideos@gmail.com>
2022-10-17 20:48:57 -0300

    add: js countdown

---
+++ js/countdown.js
@@ -0,0 +1,18 @@
+import { signOutUser } from "./logout.js";
+const countdownSeconds = document.getElementById('countdown')
+let seconds = 20;
+
+
+const countdownTimer = setInterval(() => {
+    seconds == 0 ? redirectToLoginPage() : 
+    seconds--
+    countdownSeconds.textContent = seconds
+}, 1000)
+
+
+function redirectToLoginPage() {
+    clearInterval(countdownTimer)
+    // localStorage.clear()
+    signOutUser()
+    // window.location.href = '../index.html'
+}
