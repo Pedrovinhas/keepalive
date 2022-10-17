0611848913f3e2e025060443c79fae21ebe3c64c
pedrovinhas <pstvideos@gmail.com>
2022-10-17 20:49:58 -0300

    add: js login input animation

---
+++ js/inputAnimation.js
@@ -0,0 +1,40 @@
+const username = document.getElementById('user')
+const password = document.getElementById('password')
+const passwordIcon = document.getElementById('password-icon')
+const userIcon = document.getElementById('user-icon')
+
+window.addEventListener('resize', () => {
+    if(window.screen.width >= '1024 ') {
+        username.value.length == 0 ? userIcon.style.left = '350px' : userIcon.style.left = '300px' 
+        password.value.length == 0 ? passwordIcon.style.left = '350px' : passwordIcon.style.left = '300px' 
+    } else if(window.screen.width < '1024') {
+        username.value.length == 0 ? userIcon.style.left = '230px' : userIcon.style.left = '185px' 
+        password.value.length == 0 ? passwordIcon.style.left = '230px' : passwordIcon.style.left = '185px' 
+    }
+})
+
+username.addEventListener('keyup', () => {
+        if(window.screen.width >= '1024 ') {
+            username.value.length == 0 ? 
+            userIcon.style.left = '350px'
+            :
+            userIcon.style.left = '300px'   
+        } else {
+            username.value.length == 0 ? userIcon.style.left = '230px' : userIcon.style.left = '185px' 
+        }
+       
+
+})
+
+password.addEventListener('keyup', () => {
+    if(window.screen.width >= '1024 ') {
+        password.value.length == 0 ? 
+        passwordIcon.style.left = '350px'
+        :
+        passwordIcon.style.left = '300px'   
+    } else {
+        password.value.length == 0 ? passwordIcon.style.left = '230px' : passwordIcon.style.left = '185px' 
+    }
+
+})
+
